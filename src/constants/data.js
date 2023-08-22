ee7c7ff920836fdcd793444240f4d973c798e0bc
Doleshor khadka <doleshorkhad@gmail.com>
2023-08-22 12:39:53 +0545

    added data

---
+++ src/constants/data.js
@@ -0,0 +1,32 @@
+const Cities = [
+  {
+    image: "images/paris.jpg",
+    title: "PARIS",
+    subtitle: "France",
+    description:
+      "Since the 17th century, Paris has been one of the world's major centres of finance, diplomacy, commerce, culture, fashion, gastronomy and many areas.",
+  },
+  {
+    image: "images/london.jpg",
+    title: "LONDON",
+    subtitle: "United Kingdom",
+    description:
+      "When exploring London, you will come across lots of excitement by chance, so try to take everything in rather than just rushing around to all of the major tourist haunts.",
+  },
+  {
+    image: "images/barcelona.jpg",
+    title: "BARCELONA",
+    subtitle: "Spain",
+    description:
+      "Since the 17th ceOverlooking the Mediterranean Sea, and famous for Gaudí and other Art Nouveau architecture, Barcelona is one of Europe's trendiest cities.",
+  },
+  {
+    image: "images/prague.jpg",
+    title: "prague",
+    subtitle: "Czech Republic",
+    description:
+      "Old-world charm meets modern vibrancy in Europe's capital of stunning architecture, with the iconic Charles Bridge and a captivating cultural scene.",
+  },
+];
+
+export default Cities;
